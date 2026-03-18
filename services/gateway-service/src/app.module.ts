e0323f027f8d18cd255b07134157e215ca58116b
Karolina Ścibek <karolina.scibek97@gmail.com>
2026-03-18 20:15:21 +0100

    refactor(gateway): rename services.config to proxies.config

--- services/gateway-service/src/app.module.ts
+++ services/gateway-service/src/app.module.ts
@@ -3,7 +3,7 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './modules/users/users.module';
 import { ProxiesModule } from './modules/proxies/proxies.module';
-import { config } from 'config/services.config';
+import { config } from 'configs/proxies.config';
 
 @Module({
   imports: [
